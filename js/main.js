ec215bae8b1c34f2e5941f42a3b4410a87d72749
RedDevil113 <380968113361z@gmail.com>
2017-06-15 08:26:53 +0300

    game-new

---
+++ js/main.js
@@ -0,0 +1,87 @@
+var box_pos_1 = new Box('50px','50px'),
+	box_pos_2 = new Box('200px','50px'),
+	box_pos_3 = new Box('350px','50px'),
+
+	box_pos_4 = new Box('50px','200px'),
+	box_pos_5 = new Box('200px','200px'),
+	box_pos_6 = new Box('350px','200px'),
+
+	box_pos_7 = new Box('50px','350px'),
+	box_pos_8 = new Box('200px','350px'),
+	box_pos_9 = new Box('350px','350px'),
+
+	box_pos_10 = new Box('50px','500px'),
+	box_pos_11 = new Box('200px','500px'),
+	box_pos_12 = new Box('350px','500px'),
+
+	box_pos_13 = new Box('50px','650px'),
+	box_pos_14 = new Box('200px','650px'),
+	box_pos_15 = new Box('350px','650px');
+
+
+	box1.style.left = box_pos_1.x;
+	box1.style.top = box_pos_1.y;
+
+	box2.style.left = box_pos_2.x;
+	box2.style.top = box_pos_2.y;
+
+	box3.style.left = box_pos_3.x;
+	box3.style.top = box_pos_3.y;
+
+	box4.style.left = box_pos_4.x;
+	box4.style.top = box_pos_4.y;
+
+	box5.style.left = box_pos_5.x;
+	box5.style.top = box_pos_5.y;
+
+	box6.style.left = box_pos_6.x;
+	box6.style.top = box_pos_6.y;
+
+	box7.style.left = box_pos_7.x;
+	box7.style.top = box_pos_7.y;
+
+	box8.style.left = box_pos_8.x;
+	box8.style.top = box_pos_8.y;
+
+	box9.style.left = box_pos_9.x;
+	box9.style.top = box_pos_9.y;
+
+	box10.style.left = box_pos_10.x;
+	box10.style.top = box_pos_10.y;
+
+	box11.style.left = box_pos_11.x;
+	box11.style.top = box_pos_11.y;
+
+	box12.style.left = box_pos_12.x;
+	box12.style.top = box_pos_12.y;
+
+	box13.style.left = box_pos_13.x;
+	box13.style.top = box_pos_13.y;
+
+	box14.style.left = box_pos_14.x;
+	box14.style.top = box_pos_14.y;
+
+	box15.style.left = box_pos_15.x;
+	box15.style.top = box_pos_15.y;
+
+/*
+var box_pos_1 = new Box('50px','50px'),
+	box_pos_2 = new Box('150px','50px'),
+	box_pos_3 = new Box('250px','50px'),
+
+	box_pos_4 = new Box('50px','150px'),
+	box_pos_5 = new Box('150px','150px'),
+	box_pos_6 = new Box('250px','150px'),
+
+	box_pos_7 = new Box('50px','250px'),
+	box_pos_8 = new Box('150px','250px'),
+	box_pos_9 = new Box('250px','250px'),
+
+	box_pos_10 = new Box('50px','350px'),
+	box_pos_11 = new Box('150px','350px'),
+	box_pos_12 = new Box('250px','350px'),
+
+	box_pos_13 = new Box('50px','450px'),
+	box_pos_14 = new Box('150px','450px'),
+	box_pos_15 = new Box('250px','450px');
+*/
