489e98968ee93b545a7ef88669c4758b0d8ac8fe
RedDevil113 <380968113361z@gmail.com>
2017-06-15 20:35:58 +0300

    game-3

--- js/main.js
+++ js/main.js
@@ -1,116 +1,52 @@
-function position(name,left,top) {
-	name.style.left = left + 'px';
-	name.style.top = top + 'px';
+var car = document.getElementById('car');
+
+function moveCar(top,left,deg) {
+	car.style.left = (parseInt(car.style.left) - left) + 'px';
+	car.style.top = (parseInt(car.style.top) - top) + 'px';
+	car.style.transform = "translate(-50%,-50%) rotate(" + deg + "deg)";
 }
-position(box1,50,50);
-position(box2,200,50);
-position(box3,350,50);
 
-position(box4,50,200);
-position(box5,200,200);
-position(box6,350,200);
+window.addEventListener('keyup', function(event){
+	if(event.keyCode == 37){
+		if(car.style.left != '0px' && car.style.left != '25px')
+			moveCar(0,150,90);//setTimeout(moveCar,200,0,150,90);
+	}
+	if(event.keyCode == 39){
+		if(car.style.left != '475px')
+			moveCar(0,-150,-90);//setTimeout(moveCar,200,0,-150,-90);
+	}
+	if(event.keyCode == 38){
+		if(car.style.top != '0px' && car.style.top != '25px')
+			moveCar(150,0,-180);//setTimeout(moveCar,200,150,0,-180);
+	}
+	if(event.keyCode == 40){
+		if(car.style.top != '775px')
+			moveCar(-150,0,0);//setTimeout(moveCar,200,-150,0,0);
+	}
 
-position(box7,50,350);
-position(box8,200,350);
-position(box9,350,350);
-
-position(box10,50,500);
-position(box11,200,500);
-position(box12,350,500);
-
-position(box13,50,650);
-position(box14,200,650);
-position(box15,350,650);
-/*
-function Box(x,y) {
-	this.x = x;
-	this.y = y;
-}
-		
-var box_pos_1 = new Box('50px','50px'),
-	box_pos_2 = new Box('200px','50px'),
-	box_pos_3 = new Box('350px','50px'),
-
-	box_pos_4 = new Box('50px','200px'),
-	box_pos_5 = new Box('200px','200px'),
-	box_pos_6 = new Box('350px','200px'),
-
-	box_pos_7 = new Box('50px','350px'),
-	box_pos_8 = new Box('200px','350px'),
-	box_pos_9 = new Box('350px','350px'),
-
-	box_pos_10 = new Box('50px','500px'),
-	box_pos_11 = new Box('200px','500px'),
-	box_pos_12 = new Box('350px','500px'),
-
-	box_pos_13 = new Box('50px','650px'),
-	box_pos_14 = new Box('200px','650px'),
-	box_pos_15 = new Box('350px','650px');
+	// key
+	if( car.style.left == keyO.style.left && car.style.top == keyO.style.top && keyO.classList.contains("keyO") ){
+		keyO.classList.remove("keyO");
+		car.classList.add("keyO");
+	}
+	if( car.style.left == keyY.style.left && car.style.top == keyY.style.top && keyY.classList.contains("keyY") ){
+		keyY.classList.remove("keyY");
+		car.classList.add("keyY");
+	}
+	if( car.style.left == keyG.style.left && car.style.top == keyG.style.top && keyG.classList.contains("keyG") ){
+		keyG.classList.remove("keyG");
+		car.classList.add("keyG");
+	}
+	// house
+	if( car.style.left == oHouse.style.left && car.style.top == oHouse.style.top && car.classList.contains("keyO") ){
+		oHouse.style.background = 'red';
+	}
+	if( car.style.left == yHouse.style.left && car.style.top == yHouse.style.top && car.classList.contains("keyY") ){
+		yHouse.style.background = 'red';
+	}
+	if( car.style.left == gHouse.style.left && car.style.top == gHouse.style.top && car.classList.contains("keyG") ){
+		gHouse.style.background = 'red';
+	}
 
 
-	box1.style.left = box_pos_1.x;
-	box1.style.top = box_pos_1.y;
-
-	box2.style.left = box_pos_2.x;
-	box2.style.top = box_pos_2.y;
-
-	box3.style.left = box_pos_3.x;
-	box3.style.top = box_pos_3.y;
-
-	box4.style.left = box_pos_4.x;
-	box4.style.top = box_pos_4.y;
-
-	box5.style.left = box_pos_5.x;
-	box5.style.top = box_pos_5.y;
-
-	box6.style.left = box_pos_6.x;
-	box6.style.top = box_pos_6.y;
-
-	box7.style.left = box_pos_7.x;
-	box7.style.top = box_pos_7.y;
-
-	box8.style.left = box_pos_8.x;
-	box8.style.top = box_pos_8.y;
-
-	box9.style.left = box_pos_9.x;
-	box9.style.top = box_pos_9.y;
-
-	box10.style.left = box_pos_10.x;
-	box10.style.top = box_pos_10.y;
-
-	box11.style.left = box_pos_11.x;
-	box11.style.top = box_pos_11.y;
-
-	box12.style.left = box_pos_12.x;
-	box12.style.top = box_pos_12.y;
-
-	box13.style.left = box_pos_13.x;
-	box13.style.top = box_pos_13.y;
-
-	box14.style.left = box_pos_14.x;
-	box14.style.top = box_pos_14.y;
-
-	box15.style.left = box_pos_15.x;
-	box15.style.top = box_pos_15.y;
-*/
-/*
-var box_pos_1 = new Box('50px','50px'),
-	box_pos_2 = new Box('150px','50px'),
-	box_pos_3 = new Box('250px','50px'),
-
-	box_pos_4 = new Box('50px','150px'),
-	box_pos_5 = new Box('150px','150px'),
-	box_pos_6 = new Box('250px','150px'),
-
-	box_pos_7 = new Box('50px','250px'),
-	box_pos_8 = new Box('150px','250px'),
-	box_pos_9 = new Box('250px','250px'),
-
-	box_pos_10 = new Box('50px','350px'),
-	box_pos_11 = new Box('150px','350px'),
-	box_pos_12 = new Box('250px','350px'),
-
-	box_pos_13 = new Box('50px','450px'),
-	box_pos_14 = new Box('150px','450px'),
-	box_pos_15 = new Box('250px','450px');
-*/
+});
---
+++ js/position.js
@@ -0,0 +1,150 @@
+function position(name,left,top) {
+	name.style.left = left + 'px';
+	name.style.top = top + 'px';
+}
+
+position(car,175,175);
+
+position(box1,100,100);
+position(box2,250,100);
+position(box3,400,100);
+
+position(box4,100,250);
+position(box5,250,250);
+position(box6,400,250);
+
+position(box7,100,400);
+position(box8,250,400);
+position(box9,400,400);
+
+position(box10,100,550);
+position(box11,250,550);
+position(box12,400,550);
+
+position(box13,100,700);
+position(box14,250,700);
+position(box15,400,700);
+
+//=== key ===
+position(keyO,25,25);
+position(keyY,325,325);
+position(keyG,175,625);
+
+//=== house ===
+position(oHouse,325,775);
+position(yHouse,175,475);
+position(gHouse,475,25);
+
+
+
+
+
+/*
+var map = {
+	field: [
+	 	[1,2,3],
+	 	[4,5,6]
+	],
+	pets: {
+		cat: {1,0},
+		dog: {5,6}
+	},
+	houses: {
+		cat: {4,5},
+		dog: {6,7}
+	}
+}*/
+
+/*
+function Box(x,y) {
+	this.x = x;
+	this.y = y;
+}
+
+var box_pos_1 = new Box('50px','50px'),
+	box_pos_2 = new Box('200px','50px'),
+	box_pos_3 = new Box('350px','50px'),
+
+	box_pos_4 = new Box('50px','200px'),
+	box_pos_5 = new Box('200px','200px'),
+	box_pos_6 = new Box('350px','200px'),
+
+	box_pos_7 = new Box('50px','350px'),
+	box_pos_8 = new Box('200px','350px'),
+	box_pos_9 = new Box('350px','350px'),
+
+	box_pos_10 = new Box('50px','500px'),
+	box_pos_11 = new Box('200px','500px'),
+	box_pos_12 = new Box('350px','500px'),
+
+	box_pos_13 = new Box('50px','650px'),
+	box_pos_14 = new Box('200px','650px'),
+	box_pos_15 = new Box('350px','650px');
+
+
+	box1.style.left = box_pos_1.x;
+	box1.style.top = box_pos_1.y;
+
+	box2.style.left = box_pos_2.x;
+	box2.style.top = box_pos_2.y;
+
+	box3.style.left = box_pos_3.x;
+	box3.style.top = box_pos_3.y;
+
+	box4.style.left = box_pos_4.x;
+	box4.style.top = box_pos_4.y;
+
+	box5.style.left = box_pos_5.x;
+	box5.style.top = box_pos_5.y;
+
+	box6.style.left = box_pos_6.x;
+	box6.style.top = box_pos_6.y;
+
+	box7.style.left = box_pos_7.x;
+	box7.style.top = box_pos_7.y;
+
+	box8.style.left = box_pos_8.x;
+	box8.style.top = box_pos_8.y;
+
+	box9.style.left = box_pos_9.x;
+	box9.style.top = box_pos_9.y;
+
+	box10.style.left = box_pos_10.x;
+	box10.style.top = box_pos_10.y;
+
+	box11.style.left = box_pos_11.x;
+	box11.style.top = box_pos_11.y;
+
+	box12.style.left = box_pos_12.x;
+	box12.style.top = box_pos_12.y;
+
+	box13.style.left = box_pos_13.x;
+	box13.style.top = box_pos_13.y;
+
+	box14.style.left = box_pos_14.x;
+	box14.style.top = box_pos_14.y;
+
+	box15.style.left = box_pos_15.x;
+	box15.style.top = box_pos_15.y;
+*/
+/*
+var box_pos_1 = new Box('50px','50px'),
+	box_pos_2 = new Box('150px','50px'),
+	box_pos_3 = new Box('250px','50px'),
+
+	box_pos_4 = new Box('50px','150px'),
+	box_pos_5 = new Box('150px','150px'),
+	box_pos_6 = new Box('250px','150px'),
+
+	box_pos_7 = new Box('50px','250px'),
+	box_pos_8 = new Box('150px','250px'),
+	box_pos_9 = new Box('250px','250px'),
+
+	box_pos_10 = new Box('50px','350px'),
+	box_pos_11 = new Box('150px','350px'),
+	box_pos_12 = new Box('250px','350px'),
+
+	box_pos_13 = new Box('50px','450px'),
+	box_pos_14 = new Box('150px','450px'),
+	box_pos_15 = new Box('250px','450px');
+*/
