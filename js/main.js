3b0ec1f1b869f80b5cd35e0af895e1b186d41f03
RedDevil113 <380968113361z@gmail.com>
2017-06-15 11:17:24 +0300

    game-2-3

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
+function position(name,left,top) {
+	name.style.left = left + 'px';
+	name.style.top = top + 'px';
+}
+position(box1,50,50);
+position(box2,200,50);
+position(box3,350,50);
+
+position(box4,50,200);
+position(box5,200,200);
+position(box6,350,200);
+
+position(box7,50,350);
+position(box8,200,350);
+position(box9,350,350);
+
+position(box10,50,500);
+position(box11,200,500);
+position(box12,350,500);
+
+position(box13,50,650);
+position(box14,200,650);
+position(box15,350,650);
+/*
+function Box(x,y) {
+	this.x = x;
+	this.y = y;
+}
+		
 var box_pos_1 = new Box('50px','50px'),
 	box_pos_2 = new Box('200px','50px'),
 	box_pos_3 = new Box('350px','50px'),
@@ -63,7 +92,7 @@ var box_pos_1 = new Box('50px','50px'),
 
 	box15.style.left = box_pos_15.x;
 	box15.style.top = box_pos_15.y;
-
+*/
 /*
 var box_pos_1 = new Box('50px','50px'),
 	box_pos_2 = new Box('150px','50px'),
