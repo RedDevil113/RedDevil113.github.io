1bc736efe6ee1ba6e6373e5f7d951a148d725a22
RedDevil113 <380968113361z@gmail.com>
2017-06-30 20:47:18 +0300

    site 3 sections

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
 	var head = $('header');
 
-	$('.menu').on('click','a',function(e){
-		e.preventDefault();
-		var id  = $(this).attr('href'),
-			top = $(id).offset().top;
-		$('html,body').animate({scrollTop: top - head.height()},900);
-		head.removeClass('open');
-	});
+	scrollLink('.menu a');
+	scrollLink('.banner-a');
+	
+	function scrollLink(link){
+		$(link).click(function(e){
+			e.preventDefault();
+			var id  = $(this).attr('href'),
+				top = $(id).offset().top;
+			$('html,body').animate({scrollTop: top - head.height()},900);
+			head.removeClass('open');
+		});
+	}
 });
---
+++ libs/numcount.js
@@ -0,0 +1,53 @@
+var time = 2, cc = 1;
+$(window).scroll(function(){
+$('#counter').each(function(){/*сдесь мы указываем ID который будут содержаться все блоки с прокруткой счетчика*/
+      var
+      cPos = $(this).offset().top,
+      topWindow = $(window).scrollTop();
+      if(cPos < topWindow + 200){
+        if(cc < 2){
+          $('.number').addClass('viz');/*сдесь мы указываем класс который добавляеи opacity: 1;*/
+          $('div').each(function(){
+          var 
+          i = 1,
+          num = $(this).data('num'),
+          step = 1000 * time / num,
+          that = $(this),
+          int = setInterval(function(){
+            if (i <= num) {
+              that.html(i);
+            }
+            else {
+              cc = cc + 2;
+              clearInterval(int);
+            }
+            i++;
+          },step);
+        });
+       }
+      }
+  });
+});
+
+/*Доработка для больших чисел
+{...}
+var
+    i = 1,
+    istep = 1,  // создаем переменную для шага увеличения цифр
+    num = $(this).data('num'),
+    step = 1000 * time / num,
+    that = $(this), inter;
+    if (num.toString().length > 3) // Если количество цифр больше 3
+    istep = 8; // то шаг прокрутки будет равен, например, 8
+    inter = setInterval(function () {
+    if (i <= num) {
+        that.html(i);
+    } else {
+        cc += 2;
+        that.html(num); /*! Принудительно устанавливаем конечное значение, тем самым избавляясь от погрешности расчета */
+       /* clearInterval(inter);
+    /*}
+    i+=istep; // увеличиваем на заданный шаг переменную
+}, step * istep); // умножаем для ускорения увеличения больших цифр
+{...}﻿
+*/
