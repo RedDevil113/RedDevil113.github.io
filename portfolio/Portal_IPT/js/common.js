eb29814efb07aa9ab0fac190aeff8f51aa7e87cd
Dima Hubachov <dimahubachov@Dimas-MacBook-Pro.local>
2018-02-28 15:34:59 +0200

    Portal_IPT

---
+++ portfolio/Portal_IPT/js/common.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+	$('.wf-sidebar div').on('click', function(){
+		$(this).toggleClass('div-open');
+		$(this).find('p').next().slideToggle('200');
+	});
+
+	$('.wf-grid > div').on('click', function(){
+		$(this).find('ul').slideToggle('200');
+		$(this).toggleClass('widget-open');
+	});
+
+	$('.wf-grid').masonry({
+	  itemSelector: 'article,.wf-grid > div',
+	  columnWidth: 'article',
+	  gutter: 30
+	});
+
+	//mob menu
+	$('.menu-bar').on('click', function(){
+		$('.wf-sidebar').toggleClass('sidebar-open');
+		$(this).toggleClass('bar-open');
+	});
+
+});
