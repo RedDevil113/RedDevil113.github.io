9a5e851d03b7391bfba8c375402ae4f514965069
Dima Hubachov <dimahubachov@Dimas-MacBook-Pro.local>
2018-04-05 08:37:16 +0300

    eliterate

---
+++ portfolio/eliterate/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+  $('.offer-drop p').on('click', function(){
+    $(this).parent().parent().toggleClass('offer-drop__open');
+  });
+
+  $('.top-bar__mob').on('click', function(){
+    $('.menu .container').slideToggle(400);
+  });
+
+});
