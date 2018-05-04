6261a475d07336f912d0d04afa0628745eb86bfc
Dima Hubachov <dimahubachov@Dimas-MacBook-Pro.local>
2018-05-04 09:45:51 +0300

    wwdm sidebar

--- portfolio/wwdm/js/common.js
+++ portfolio/wwdm/js/common.js
@@ -1,15 +1,17 @@
 $(document).ready(function(){
 
-	 $('select').selectize({});
-
+	$('select').selectize({});
 
 	$('.header-user__img').on('click', function(){
+		$(this).toggleClass('user-open__img');
 		$('.header-user ul').toggleClass('user-open');
+		$('.header-right__menu-drop ul').removeClass('drop-open');
 	});
 
 	$('.header-right__menu-drop').on('click', function(e){
 		e.preventDefault();
 		$('.header-right__menu-drop ul').toggleClass('drop-open');
+		$('.header-user ul').removeClass('user-open');
 	});
 
 	$('.menu-bar').on('click', function(){
