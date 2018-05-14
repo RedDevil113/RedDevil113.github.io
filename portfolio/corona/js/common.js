5b64fcaa46111a3612cd1eb2a4fccc7bb1458c65
Dima Hubachov <dimahubachov@Dimas-MacBook-Pro.local>
2018-05-14 11:58:37 +0300

    corona header

--- portfolio/corona/js/common.js
+++ portfolio/corona/js/common.js
@@ -1,4 +1,34 @@
+var video = {
+
+  init: function() {
+    video.play();
+  },
+
+  play: function() {
+    jQuery('body').on('click', '#play-video-btn', function(e) {
+      video.setSrc(this);
+    });
+  },
+
+  setSrc: function(el) {
+    if (el) {
+      var video_id = jQuery(el).data('id');
+      var url = '//www.youtube.com/embed/' + video_id + '?enablejsapi=1&version=3&showinfo=0&playerapiid=ytplayer&autoplay=1';
+      jQuery('img#embed-video-preview').hide();
+      jQuery('#play-video-btn').hide();
+      jQuery('iframe#embed-video-frame').attr('src', url).fadeIn();
+    }
+  }
+}
+
 $(document).ready(function(){
+
+	$('select').selectize({});
+
+	//mob menu
+	$('.menu-bar,.arrow-mob').on('click', function(){
+		$('.header').toggleClass('open-menu');
+	});
 
 	function titlePadding(){
 		var content = $('.article-col__wrap').each(function(i,elem){
@@ -12,5 +42,16 @@ $(document).ready(function(){
 	$(window).resize(function(){
 		titlePadding();
 	});
+
+	$(window).scroll(function(){
+		if($(this).width() <= 860){
+			if($(this).scrollTop() > 30)
+				$('.header').addClass('scroll-header');
+			else
+				$('.header').removeClass('scroll-header');
+		}
+	});
+
+	video.init();
 
 });
