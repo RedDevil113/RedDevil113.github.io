65ba17436d1ec6f407b26e63b409eff055880bb7
Dmitriy Hubachov <gubacevdima@gmail.com>
2018-08-22 20:10:54 +0300

    caesar-new

---
+++ portfolio/caesar-new/js/common.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+
+	$('.menu-bar').on('click', function(){
+		$('.header-menu').slideToggle();
+	});
+
+	$('.news-slider__s').slick({
+	  infinite: false,
+	  arrows: true,
+	  dots: false,
+	  nextArrow: '<button class="slick-arrow slick-next"></button>',
+ 	  prevArrow: '<button class="slick-arrow slick-prev"></button>',
+	  slidesToShow: 2,
+	  variableWidth: true,
+	  slidesToScroll: 1,
+	  speed:1000,
+	  responsive: [
+    {
+      breakpoint: 450,
+      settings: {
+        variableWidth: false,
+        slidesToShow: 1
+      }
+    },
+    ]
+	});
+
+	//s;ider range
+  if($( "#slider-range__1" ).length != 0){
+  	$( "#slider-range__1" ).slider({
+      range: true,
+      min: 0,
+      max: 10000,
+      values: [ 3000, 6000 ],
+      slide: function( event, ui ) {
+        $("#search-price__1").val( ui.values[ 0 ]);
+        $("#search-price__2").val( ui.values[ 1 ]);
+      }
+    });
+    $("#search-price__1").val( $( "#slider-range__1" ).slider( "values", 0 ));
+    $("#search-price__2").val( $( "#slider-range__1" ).slider( "values", 1 ));
+  }
+
+  //accordeons
+	$(".accordeon .accordeon-descr").hide().prev().click(function() {
+
+		if($(this).parents(".accordeon").hasClass('accordeon-active'))
+			$(".accordeon").removeClass('accordeon-active');
+		else {
+			$(".accordeon").removeClass('accordeon-active');
+			$(this).parents(".accordeon").addClass('accordeon-active');
+		}
+
+		$(this).parents().find(".accordeon .accordeon-descr").not(this).slideUp().prev();
+		$(this).next().not(":visible").slideDown().prev().parents();
+
+	});
+	$(".accordeon-open").addClass('accordeon-active').find(".accordeon-descr").show();
+
+	$('.catalog-sidebar__list .catalog-sidebar__title').on('click', function(){
+		if($(window).width() < 768){
+			$('.catalog-sidebar__list').toggleClass('catalog-sidebar__list-open');
+			$('.catalog-sidebar__list ul').slideToggle();
+		}
+	});
+
+	$('.catalog-sidebar__button').on('click', function(){
+		$('.catalog-sidebar__filter').toggleClass('catalog-sidebar__filter-open');
+	});
+
+});
