02179947e82cd8e8b9ac25a5d14f966dd3aa5358
Dmitriy Hubachov <gubacevdima@gmail.com>
2019-12-09 15:00:21 +0200

    bumper sidebar 2.0

--- portfolio/bumper/js/checkout.js
+++ portfolio/bumper/js/checkout.js
@@ -10,13 +10,13 @@ $(document).ready(function(){
 		console.log($(window).scrollTop());
 		if($(window).scrollTop() >= sidebarPos){
 			if($(window).scrollTop() >= block){
-				$(sidebar).removeClass('sidebar-fixed').css('left', '0');
+				$(sidebar).removeClass('sidebar-fixed').addClass('sidebar-absolute').css('left', '0');
 			}
 			else
-				$(sidebar).addClass('sidebar-fixed').css('left', ($(window).innerWidth() - $('.wrapper').innerWidth()) / 2 + 15 + 'px');
+				$(sidebar).addClass('sidebar-fixed').removeClass('sidebar-absolute').css('left', ($(window).innerWidth() - $('.wrapper').innerWidth()) / 2 + 15 + 'px');
 		}
 		else
-			$(sidebar).removeClass('sidebar-fixed').css('left', '0');
+			$(sidebar).removeClass('sidebar-fixed').removeClass('sidebar-absolute').css('left', '0');
 	});
 
 });
